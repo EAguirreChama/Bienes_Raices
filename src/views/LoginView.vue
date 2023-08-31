77f421bd692b3c1b5bcd1a5a9c98e7b96dcf716a
EAguirre <aguirreernesto25@gmail.com>
2023-08-31 11:51:47 -0300

    Formulario y Validaciones

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,4 +1,15 @@
 <script setup>
+    import { useForm, useField} from 'vee-validate'
+    import { loginSchema as validationSchema } from '../validation/loginSchema'
+
+    const { handleSubmit } = useForm({ validationSchema })
+
+    const email = useField('email')
+    const password = useField('password')
+
+    const submit = handleSubmit(() => {
+
+    })
 
 </script>
 
@@ -18,15 +29,22 @@
                 type="email"
                 label="Email"
                 bg-color="blue-grey-lighten-5"
+                class="mb-3"
+                v-model="email.value.value"
+                :error-messages="email.errorMessage.value"
             />
             <v-text-field
                 type="password"
                 label="Password"
                 bg-color="blue-grey-lighten-5"
+                class="mb-3"
+                v-model="password.value.value"
+                :error-messages="password.errorMessage.value"
             />
             <v-btn
                 block
                 color="pink-accent-3"
+                @click="submit"
             >
                 Iniciar Sesión
             </v-btn>
