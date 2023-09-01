a8e9ca540ea783a3711da3c8ad97621ba464fc56
EAguirre <aguirreernesto25@gmail.com>
2023-09-01 11:44:01 -0300

    Restringiendo accesos

--- src/router/index.js
+++ src/router/index.js
@@ -18,25 +18,40 @@ const router = createRouter({
       path: '/admin',
       name: 'admin',
       component: () => import ('../views/admin/AdminLayout.vue'),
+      meta: { requireAuth: true},
       children: [
         {
           path: '/admin/propiedades',
           name: 'admin-propiedades',
-          component: () => import ('../views/admin/AdminView.vue')
+          component: () => import ('../views/admin/AdminView.vue'),
+        meta: { requireAuth: true}
         },
         {
           path: '/admin/nueva',
           name: 'nueva-propiedad',
-          component: () => import ('../views/admin/NuevaPropiedadView.vue')
+          component: () => import ('../views/admin/NuevaPropiedadView.vue'),
+          meta: { requireAuth: true}
         },
         {
           path: '/admin/editar/:id',
           name: 'editar-propiedad',
-          component: () => import ('../views/admin/EditarPropiedadView.vue')
+          component: () => import ('../views/admin/EditarPropiedadView.vue'),
+          meta: { requireAuth: true}
         }
       ]
     }
   ]
+})
+
+// Guard de navegación
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(url => url.meta.requireAuth)
+  if(requiresAuth){
+    // Comprobar que el usuario este autenticado
+  } else {
+    // No esta protegido
+    next()
+  }
 })
 
 export default router
