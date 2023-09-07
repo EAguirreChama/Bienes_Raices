e2306cca0c0d342750ebd33cb43effcf98ec145e
EAguirre <aguirreernesto25@gmail.com>
2023-09-07 17:32:12 -0300

    Se muestran, ahora a cambiar la forma del precio

--- src/views/admin/AdminView.vue
+++ src/views/admin/AdminView.vue
@@ -14,4 +14,28 @@
     >
         Nueva Propiedad
     </v-btn>
+
+    <v-card class="mx-auto mt-10">
+        <v-list>
+            <v-list-item v-for="propiedad in propiedadesCollection" :key="propiedad.id"> 
+                <template v-slot:prepend>
+                    <v-list-item-media start="true">
+                        <img :src="propiedad.imagen" width="180">
+                    </v-list-item-media>
+                </template>
+
+                <v-list-item-title>{{ propiedad.titulo }}</v-list-item-title>
+                <v-list-item-subtitle>{{ propiedad.precio }}</v-list-item-subtitle>
+
+                <template v-slot:append>
+                    <v-btn color="info" flat class="mr-2">
+                        Editar
+                    </v-btn>
+                    <v-btn color="red-darken-3" flat>
+                        Eliminar
+                    </v-btn>
+                </template>
+            </v-list-item>
+        </v-list>
+    </v-card>
 </template>
