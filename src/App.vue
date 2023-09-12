fb7c0b2eac1bdd2ddede6339fc212d6ef7c80894
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-12 19:17:05 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,22 +6,16 @@
 </script>
 
 <template>
-  <v-card
-    elevation="3"
-    max-width="1200"
-    class="mx-auto"
-  >
+  <v-card elevation="3" max-width="1200" class="mx-auto">
     <v-layout>
-      <v-app-bar
-         color="blue-darken-1"
-         >
+      <v-app-bar color="blue-darken-1">
           <template v-slot:prepend>
             <v-btn :to="{name: 'home'}">
               Bienes Raices
             </v-btn>
           </template>
+        
           <template v-slot:append>
-
             <div v-if="auth.isAuth">
               <v-btn :to="{name: 'admin-propiedades'}">Admin</v-btn>
               <v-btn @click="auth.logout">Cerrar Sesión</v-btn>
@@ -31,7 +25,6 @@
               <v-btn :to="{name: 'home'}">Inicio</v-btn>
               <v-btn :to="{name: 'login'}">Iniciar Sesión</v-btn>
             </div>
-
           </template>
       </v-app-bar>
 
@@ -40,7 +33,6 @@
           <RouterView/>
         </v-container>
       </v-main>
-
     </v-layout>
   </v-card>
 </template>
